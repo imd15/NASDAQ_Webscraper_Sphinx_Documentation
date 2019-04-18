108bef66e469b4c8269114050efce62f6a26e3a5
imd15 <imd15@my.fsu.edu>
2019-04-18 18:26:49 -0400

    Added support for argparse(docopt)

--- driver.py
+++ driver.py
@@ -1,7 +1,22 @@
+"""
+    Usage:
+        driver.py --operation=<Ticker> [--ticker_count=<num>]
+        driver.py --operation=<Fetcher> [--ticker_count=<num>] [--time_limit=<time_lim>] [--db=<dbname>]
+        driver.py --operation=<Query> [--time=<time>] [--db=<dbname>] [--ticker=<ticker_name>]
+
+    Options:
+        --operation=<Ticker> Get Tickers
+        --ticker_count=<num> num of tickers [default: 100]
+        --time_limit=<time_lim> time limit for fetcher [default: 100]
+        --db=<dbname> database name [default: "stocks.db"]
+
+"""
 import sys
 import re
 from stock import Tickers, Fetcher, Query
 import argparse as ap
+from docopt import docopt
+
 
 if __name__ == "__main__":
     """
@@ -17,32 +32,49 @@ if __name__ == "__main__":
     #Kiel.fetch_all_data(5)
     #test = Query("YI","17:33")
     #test.Get_Datails()
-
+    
+    args = docopt(__doc__)
     op = sys.argv[1]
-    operation = (sys.argv[1]).split("=")[1]
+    operation = args['--operation']
+    tickernum = args['--ticker_count']
+    time_lim = args['--time_limit']
+    curr_time = args['--time']
+    ticker= args['--ticker']
+    dbname= args['--db']
+
+    #These set the default
+    if tickernum == None:
+        tickernum = 110
+    if ticker == None:
+        ticker = 'YI'
+    if dbname == None:
+        dbname = "stocks_new.db"
+    if curr_time == None:
+        curr_time = "16:32"
+    if time_lim == None:
+        time_lim = 120
+
 
     if operation == "Ticker":
-        ticker_count =  int((sys.argv[2]).split("=")[1])
+        ticker_count =  tickernum
         #print(ticker_count)
         ticker_call = Tickers(ticker_count,"tickers.txt")
         ticker_call.save_tickers()
 
     elif operation == "Fetcher":
-        fetcher_count =  (sys.argv[2]).split("=")[1]
-        time_count =  int((sys.argv[3]).split("=")[1])
-        database = (sys.argv[4]).split("=")[1]
+        fetcher_count =  int(tickernum)
+        time_count =  int(time_lim)
+        database = dbname
         # print(f"\nfetcher_count = {fetcher_count}\ntime_count = {time_count}\ndatabase = {database}\n")
         fetcher_call = Fetcher("tickers.txt")
         fetcher_call.fetch_all_data(time_count,database)
     
     elif operation == "Query":
-        time_count = (sys.argv[2]).split("=")[1]
-        database = (sys.argv[3]).split("=")[1]
-        ticker_name = re.sub("'",'', (sys.argv[4]).split("=")[1])
-        ticker_name = ticker_name[1:]
-        ticker_name = ticker_name[:-1]
+        time_count = curr_time
+        database = dbname
+        
         # print(f"time_count = {time_count}\ndatabase = {database}\nticker_name = {ticker_name}")
 
         #print(ticker_name)
-        query_call = Query(ticker_name,time_count,database)
+        query_call = Query(ticker,time_count,database)
         query_call.Get_Datails()
